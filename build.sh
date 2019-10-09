b34c08f132e44f6baa2365117ebeac2730b102fc
wulan17 <galihgustip@gmail.com>
2019-10-09 20:55:49 +0700

    Fix Some Typo
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y liblz4-dev openjdk-8-jdk android-tools-adb bc bison build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev
 
 # Main Environment
-KERNEL_DIR=$(pdw)
+KERNEL_DIR=$(pwd)
 KERN_IMG=$KERNEL_DIR/kernel/out/arch/arm/boot/zImage-dtb
 ZIP_DIR=$KERNEL_DIR/AnyKernel
 CONFIG_DIR=$KERNEL_DIR/kernel/arch/arm/configs
