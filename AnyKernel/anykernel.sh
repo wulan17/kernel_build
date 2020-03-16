e444f4157a32314969c4ef998394f18ababde104
wulan17 <galihgustip@gmail.com>
2020-03-17 01:50:19 +0700

    Initial commit
    Signed-off-by: wulan17 <galihgustip@gmail.com>

---
+++ AnyKernel/anykernel.sh
@@ -0,0 +1,59 @@
+
+# shell variables
+block=/dev/block/platform/bootdevice/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+kernel=/tmp/anykernel/zImage;
+
+chmod -R 755 $bin;
+mkdir $split_img;
+
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/split_img/boot.img-ramdisk.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting...";
+    echo 1 > /tmp/anykernel/exitcode; exit;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+dump_boot;
+
+write_boot;
+
+## end install
+
---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+sudo apt update && sudo apt install ccache
+# Export
+export TELEGRAM_TOKEN
+export TELEGRAM_CHAT
+export ARCH="arm"
+export SUBARCH="arm"
+export PATH="/usr/lib/ccache:$PATH"
+export KBUILD_BUILD_USER="wulan17"
+export KBUILD_BUILD_HOST="Github"
+export branch="pie"
+export device="cactus"
+export LOCALVERSION="-wulan17"
+export kernel_repo="https://github.com/wulan17/android_kernel_xiaomi_mt6765.git"
+export tc_repo="https://github.com/wulan17/linaro_arm-linux-gnueabihf-7.5.git"
+export tc_name="arm-linux-gnueabihf"
+export tc_v="7.5"
+export zip_name="kernel-""$device""-"$(env TZ='Asia/Jakarta' date +%Y%m%d)""
+export KERNEL_DIR=$(pwd)
+export KERN_IMG="$KERNEL_DIR"/kernel/out/arch/"$ARCH"/boot/zImage-dtb
+export ZIP_DIR="$KERNEL_DIR"/AnyKernel
+export CONFIG_DIR="$KERNEL_DIR"/kernel/arch/"$ARCH"/configs
+export CORES=$(grep -c ^processor /proc/cpuinfo)
+export THREAD="-j$CORES"
+CROSS_COMPILE+="ccache "
+CROSS_COMPILE+="$KERNEL_DIR"/"$tc_name"-"$tc_v"/bin/"$tc_name-"
+export CROSS_COMPILE
+
+function sync(){
+	SYNC_START=$(date +"%s")
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage
+	cd "$KERNEL_DIR" && git clone -b "$branch" "$kernel_repo" --depth 1 kernel
+	cd "$KERNEL_DIR" && git clone "$tc_repo" "$tc_name"-"$tc_v"
+	chmod -R a+x "$KERNEL_DIR"/"$tc_name"-"$tc_v"
+	SYNC_END=$(date +"%s")
+	SYNC_DIFF=$((SYNC_END - SYNC_START))
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
+}
+function build(){
+	BUILD_START=$(date +"%s")
+	cd "$KERNEL_DIR"/kernel
+	export last_tag=$(git log -1 --oneline)
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Build Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
+	make  O=out "$device"_defconfig "$THREAD" > "$KERNEL_DIR"/kernel.log
+	make "$THREAD" O=out >> "$KERNEL_DIR"/kernel.log
+	BUILD_END=$(date +"%s")
+	BUILD_DIFF=$((BUILD_END - BUILD_START))
+	export BUILD_DIFF
+}
+function success(){
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$ZIP_DIR"/"$zip_name".zip -F "parse_mode=html" -F caption="Build completed successfully in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))
+	Dev : ""$KBUILD_BUILD_USER""
+	Product : Kernel
+	Device : #""$device""
+	Branch : ""$branch""
+	Host : ""$KBUILD_BUILD_HOST""
+	Commit : ""$last_tag""
+	Compiler : ""$(${CROSS_COMPILE}gcc --version | head -n 1)""
+	Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
+	
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$KERNEL_DIR"/kernel.log https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument > /dev/null
+	exit 0
+}
+function failed(){
+	curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$KERNEL_DIR"/kernel.log -F "parse_mode=html" -F "caption=Build failed in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument > /dev/null
+	exit 1
+}
+function check_build(){
+	if [ -e "$KERN_IMG" ]; then
+		cp "$KERN_IMG" "$ZIP_DIR"
+		cd "$ZIP_DIR"
+		mv zImage-dtb zImage
+		zip -r "$zip_name".zip ./*
+		success
+	else
+		failed
+	fi
+}
+function main(){
+	sync
+	build
+	check_build
+}
+
+main
