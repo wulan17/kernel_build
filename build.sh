89a15caf67975b9969e248c7981b6f0580194f00
wulan17 <galihgustip@gmail.com>
2021-11-24 07:50:27 +0000

    dandelion -> selene
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -8,17 +8,17 @@ export ARCH="arm64"
 export SUBARCH="arm64"
 export KBUILD_BUILD_USER="wulan17"
 export KBUILD_BUILD_HOST="Github"
-export branch="10"
-export device="dandelion"
+export branch="11"
+export device="selene"
 export LOCALVERSION="-wulan17"
-export kernel_repo="https://github.com/wulan17/android_kernel_xiaomi_mt6765g.git"
+export kernel_repo="https://github.com/kbt69/android_kernel_xiaomi_selene.git"
 export tc_repo="https://github.com/wulan17/linaro_aarch64-linux-gnu-7.5.git"
 export tc_name="aarch64-linux-gnu"
 export tc32_repo="https://github.com/wulan17/linaro_arm-linux-gnueabihf-7.5.git"
 export tc32_name="arm-linux-gnueabihf"
 export tc_branch="master"
 export tc_v="7.5"
-export clang_url="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-9.0.0_r55/clang-4691093.tar.gz"
+export clang_url="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-11.0.0_r46/clang-r383902b.tar.gz"
 export clang_triple="aarch64-linux-gnu-"
 export zip_name="kernel-""$device""-"$(env TZ='Asia/Jakarta' date +%Y%m%d)""
 export KERNEL_DIR=$(pwd)
@@ -39,31 +39,24 @@ export PATH="$CC_dir:$CC32_dir:$KERNEL_DIR/clang/bin:/usr/lib/ccache:$PATH"
 
 function sync(){
 	SYNC_START=$(date +"%s")
-	curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage
+	#curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage
 	cd "$KERNEL_DIR" && git clone --quiet "$THREAD" -b "$branch" "$kernel_repo" --depth 1 kernel > /dev/null
 	cd "$KERNEL_DIR" && git clone --quiet "$THREAD" -b "$tc_branch" "$tc_repo" --depth 1 "$tc_name"-"$tc_v" > /dev/null
 	cd "$KERNEL_DIR" && git clone --quiet "$THREAD" -b "$tc_branch" "$tc32_repo" --depth 1 "$tc32_name"-"$tc_v" > /dev/null
 	wget -q "$clang_url"
 	mkdir -p clang
-	cd clang && tar -xzf ../clang-4691093.tar.gz
-	cd "$KERNEL_DIR" && rm clang-4691093.tar.gz
-	rm "$KERNEL_DIR"/kernel/kernel/module.c
-	cp module.c "$KERNEL_DIR"/kernel/kernel/
+	cd clang && tar -xzf ../clang-r383902b.tar.gz
+	cd "$KERNEL_DIR" && rm clang-r383902b.tar.gz
 	chmod -R a+x "$KERNEL_DIR"/"$tc_name"-"$tc_v"
 	SYNC_END=$(date +"%s")
 	SYNC_DIFF=$((SYNC_END - SYNC_START))
-	curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
+	#curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
 }
 function build(){
 	BUILD_START=$(date +"%s")
 	cd "$KERNEL_DIR"/kernel
 	export last_tag=$(git log -1 --oneline)
-	curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Build Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
-	#sed -i 's,CONFIG_TOUCHSCREEN_FTS=y,# CONFIG_TOUCHSCREEN_FTS is not set,g' arch/arm64/configs/dandelion_defconfig
-	mkdir drivers/input/touchscreen/mediatek/ft8006s_spi/include
-	mkdir drivers/input/touchscreen/mediatek/ft8006s_spi/include/firmware
-	touch drivers/input/touchscreen/mediatek/ft8006s_spi/include/firmware/fw_helitai_v0e.i
-	touch drivers/input/touchscreen/mediatek/ft8006s_spi/include/firmware/fw_sample.i
+	#curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Build Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
 	script "$KERNEL_DIR"/kernel.log -c 'make O=out '"$device"'_defconfig '"$THREAD"' && make '"$THREAD"' CC=clang CLANG_TRIPLE='"$clang_triple"' CROSS_COMPILE='"$tc_name"'- CROSS_COMPILE_ARM32='"$tc32_name"'- O=out'
 	BUILD_END=$(date +"%s")
 	BUILD_DIFF=$((BUILD_END - BUILD_START))
@@ -79,7 +72,7 @@ function success(){
 	Commit : ""$last_tag""
 	Compiler : ""$(${CROSS_COMPILE}gcc --version | head -n 1)""
 	Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
-	
+
 	curl -s -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$KERNEL_DIR"/kernel.log https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument > /dev/null
 	exit 0
 }
@@ -98,7 +91,7 @@ function check_build(){
 	fi
 }
 function main(){
-	curl -s -F "chat_id=$TELEGRAM_CHAT" -F "sticker=$sticker" https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendSticker > /dev/null
+	#curl -s -F "chat_id=$TELEGRAM_CHAT" -F "sticker=$sticker" https://api.telegram.org/bot"$TELEGRAM_TOKEN"/sendSticker > /dev/null
 	sync
 	build
 	check_build
