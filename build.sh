d5b312e7b62228d7fc1f654ae80fb13c1d6b46f1
wulan17 <wulan17@komodos.id>
2026-09-10 15:46:53 +0700

    kernel_build: Bring back susfs
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- build.sh
+++ build.sh
@@ -71,6 +71,12 @@ function do_patches(){
 		python main.py append_config "ksu"
 		cd kernel
 		curl https://raw.githubusercontent.com/maxsteeel/nomount/refs/heads/master/kernel/setup.sh | bash -s master
+	elif [[ "$B_TYPE" == "susfs" ]]; then
+		git -C kernel am --3way "$BASE_DIR"/patches/0001-gale-ReSukiSU-manual-hook.patch || { echo "Patch application failed!"; exit 1; }
+		git -C kernel am --3way "$BASE_DIR"/patches/0002-gale-Susfs-patch.patch || { echo "Patch application failed!"; exit 1; }
+		python main.py append_config "susfs"
+		cd kernel
+		curl https://raw.githubusercontent.com/maxsteeel/nomount/refs/heads/master/kernel/setup.sh | bash -s master
 	fi
 
 	if [[ "$SPOOF" == "True" || "$SPOOF" == "true" ]]; then
@@ -111,8 +117,10 @@ function do_anykernel(){
 			echo "Skipping dtbo.img as SHIP_DTBO is set to 0"
 		fi
 		cd "$ZIP_DIR"
-		if [[ "$B_TYPE" == "ksu" || "$B_TYPE" == "ksu-spoof" ]]; then
+		if [[ "$B_TYPE" == "ksu" ]]; then
 			sed -i "s#kernel.string=#kernel.string=$KERNEL_NAME kernel ReSukiSU for $DEVICE#g" anykernel.sh
+		elif [[ "$B_TYPE" == "susfs" ]]; then
+			sed -i "s#kernel.string=#kernel.string=$KERNEL_NAME kernel ReSukiSU+SusFS for $DEVICE#g" anykernel.sh
 		else
 			sed -i "s#kernel.string=#kernel.string=$KERNEL_NAME kernel for $DEVICE#g" anykernel.sh
 		fi
